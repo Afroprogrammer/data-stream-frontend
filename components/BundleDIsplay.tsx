b2a5aec132a7ff5095fde1ea9eb8a7b8893653d3
Franklin <forsonfranklin@gmail.com>
2022-10-06 14:50:27 +0000

    fix authentication bug

---
+++ components/BundleDIsplay.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AirtimeResourceDisplay from "./AirtimeResourceDisplay";
+import BundleResourceDisplay from "./BundleResourceDisplay";
+
+const resources = [
+    {
+
+        title: 'Data',
+        products: [
+            {
+                airtimeProvider: '9mobile Data',
+                imageUrl:  '9mobile.svg',
+            },
+            {
+                airtimeProvider: 'Airtel Data',
+                imageUrl:  'airtel.svg',
+            },
+            {
+                airtimeProvider: 'Glo Data',
+                imageUrl:  'glo.png',
+            },
+            {
+                airtimeProvider: 'MTN Data',
+                imageUrl:  'mtn.svg',
+            }
+
+
+        ]
+    }
+]
+
+function BundleDisplay() {
+    return (
+        <div>
+            {resources.map((resource) => (
+                <BundleResourceDisplay resource={resource}/>
+
+            ))}
+        </div>
+    );
+}
+
+export default BundleDisplay;
